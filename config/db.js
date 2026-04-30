134b66b50f52561faa4e2a10c4110683180bfb1d
lallahh <lallahmokgosi@gmail.com>
2026-04-30 21:20:18 +0200

    fix mssql driver for render

--- config/db.js
+++ config/db.js
@@ -1,21 +1,18 @@
-const sql = require("mssql/msnodesqlv8");
+const sql = require("mssql");
 
 const config = {
-  connectionString:
-    "Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-NK3B13Q\\SQLEXPRESS;Database=TaskBuddy;Trusted_Connection=Yes;"
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
+  options: {
+    encrypt: true,
+    trustServerCertificate: true
+  }
 };
 
-const poolPromise = new sql.ConnectionPool(config)
-  .connect()
-  .then(pool => {
-    console.log("SQL Server Connected Successfully");
-    return pool;
-  })
-  .catch(err => {
-    console.log("Database connection failed:", err);
-  });
+sql.connect(config)
+  .then(() => console.log("SQL Server Connected"))
+  .catch(err => console.log("DB Connection Failed:", err));
 
-module.exports = {
-  sql,
-  poolPromise
-};
+module.exports = sql;
